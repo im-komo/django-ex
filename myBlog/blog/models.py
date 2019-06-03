5c93353906c6034d64a1b2e3c524222862ae9133
im-komo <pravesh.tlabs@gmail.com>
2019-06-03 12:00:59 +0530

    add admin panel and update model

--- myBlog/blog/models.py
+++ myBlog/blog/models.py
@@ -4,11 +4,14 @@ from django.utils import timezone
 
 # Create your models here.
 class Post(models.Model):
+    TAGS = (('T', 'Technology'), ('F', 'Food'))
     author =models.ForeignKey(settings.AUTH_USER_MODEL, on_delete=models.CASCADE)
     title = models.CharField(max_length=200)
     text = models.TextField()
+    tags = models.CharField(max_length=1, choices=TAGS)
     created_date = models.DateTimeField(default=timezone.now)
     published_date = models.DateTimeField(blank=True, null=True)
+    ispublished = models.BooleanField(default=False)
 
     def publish(self):
         self.published_date = timezone.now()
