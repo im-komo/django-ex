efc43adb2affc144a069d8db0c901064a498b4df
im-komo <pravesh.tlabs@gmail.com>
2019-06-03 12:30:41 +0530

    last commit day 1

--- myBlog/task/urls.py
+++ myBlog/task/urls.py
@@ -2,5 +2,6 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path('', views.task_list, name='task_list'),
+    # path('', views.task_list, name='task_list'),
+    path('', views.hellotask)
 ]
