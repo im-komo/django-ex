bfb6679677e1255d3f712ca03368f134c3282586
im-komo <pravesh.tlabs@gmail.com>
2019-06-04 12:01:20 +0530

    add detail page

--- myBlog/blog/views.py
+++ myBlog/blog/views.py
@@ -1,6 +1,7 @@
 from django.shortcuts import render
 from django.http import HttpResponse
 from django.utils import timezone
+from django.shortcuts import render, get_object_or_404
 from blog.models import Post
 
 # Create your views here.
@@ -10,3 +11,8 @@ from blog.models import Post
 def post_list(request):
     posts = Post.objects.filter(published_date__lte=timezone.now()).order_by('published_date')
     return render(request, 'blog/post_list.html', {'posts': posts})
+
+def post_detail(request, pk):
+    post = get_object_or_404(Post, pk=pk)
+    print(post)
+    return render(request, 'blog/post_detail.html', {'post':post})
