1ee2b2f292ba53eaa19c9ec1f3331d06d98cf09c
im-komo <pravesh.tlabs@gmail.com>
2019-06-04 12:09:12 +0530

    update task url

--- myBlog/task/urls.py
+++ myBlog/task/urls.py
@@ -2,6 +2,5 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    # path('', views.task_list, name='task_list'),
-    path('', views.hellotask)
+   path('', views.hellotask),
 ]
